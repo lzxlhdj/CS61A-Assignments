1c48f3969e44123612b4b73f3f1ca3ee0fccb4ab
Lizi <2551181@tongji.edu.cn>
2026-02-01 22:56:41 +0800

    finished hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,17 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) (cond ((null? s) #t)
+                             ((null? (cdr s)) #t)
+                             ((> (car s) (car(cdr s))) #f)
+                             (else (ascending? (cdr s))) ))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) (cond ((null? s) nil)
+                                 (else (append (if (pred (car s)) (list (car s)) nil)
+                                               (my-filter pred (cdr s))) )    ))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) (cond ((null? lst1) lst2)
+                                     ((null? lst2) lst1)
+                                     (else (append (list (car lst1) (car lst2))
+                                                   (interleave (cdr lst1) (cdr lst2))))      ))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) (cond ((null? s) nil)
+                             (else (append (list (car s))
+                                     (no-repeats (my-filter (lambda (x) (not (= x (car s))) ) (cdr s))) ))))
